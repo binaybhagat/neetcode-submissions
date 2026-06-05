97a218a5a321328b648fe12c3d1ac7961af914ad
BinayBhagat <binaybhagat@gmail.com>
2026-06-04 22:47:18 -0700

    Add: python-variable-types - submission-1

---
+++ Python For Beginners/python-variable-types/submission-1.py
@@ -0,0 +1,15 @@
+integer_type = 42
+float_type = 56.8
+bool_type = False
+string_type = "XYZ"
+list_type = ["A",'B']
+
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
