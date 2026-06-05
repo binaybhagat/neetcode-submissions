d3265b18aacca6fa877b5c5ad07a61d6dff35db3
BinayBhagat <binaybhagat@gmail.com>
2026-06-04 22:50:07 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.9
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "ABC"
+print(type(variable))
+variable = [10.9, 2.3]
+print(type(variable))
